1739d3dc0901de967d5576358ee147396f8b57ce
ADRIANOSANCHEZ24 <128929371+ADRIANOSANCHEZ24@users.noreply.github.com>
2023-04-19 19:55:27 -0500

    Add files via upload

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈', body: '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', sourceUrl: `https://github.com/GataNina-Li/GataBot-MD`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '𝙈𝙖𝙠𝙖𝙣𝙖𝙠𝙮𝘽𝙤𝙩-𝙈𝘿', body: '𝘼𝙙𝙧𝙞𝙖𝙣𝙤 𝙎𝙖́𝙣𝙘𝙝𝙚𝙯', sourceUrl: `https://github.com/ADRIANOSANCHEZ24/MakanakyBot-MD`, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
